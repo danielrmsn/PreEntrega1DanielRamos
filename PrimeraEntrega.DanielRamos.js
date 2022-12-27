ee49293b684be6cf9697bc47c172f2ae7ef9c857
Daniel Ramos <crayolagym@gmail.com>
2022-12-26 19:46:34 -0600

    Pre entrega 1

---
+++ PrimeraEntrega.DanielRamos.js
@@ -0,0 +1,12 @@
+let productoUno = 10
+let productoDos = 20
+let productoTres = 30
+const iva = 1.16
+const total = 0
+
+function costo (producto,iva){
+    total = producto + iva
+}
+
+alert("Elige un producto de la lista: 1.-productoUno 2.-productoDos 3.-productoTres")
+
